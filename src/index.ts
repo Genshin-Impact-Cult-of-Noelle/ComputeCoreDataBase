428257043d03b8e6034bcc5b890694c71a67132e
YueAo7 <leaoqi@outlook.com>
2022-01-25 18:44:40 +0800

    ✨ feat(导出模块): 删除
    现在会自动注入<noelle-core>中

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Date: 2022-01-21 16:30:21
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-24 17:09:24
- * @FilePath: \noelleCoreDataBase\src\index.ts
+ * @LastEditTime: 2022-01-25 18:43:27
+ * @FilePath: \noelle-core-db\src\index.ts
  */
 import Noelle from "noelle-core"
 const t =  require.context("./",true,/^(?!index).*\.ts/)
@@ -14,6 +14,5 @@ const DB:any[] = []
 Object.keys(DBT).map(item=>{
     DB.push(DBT[item])
 });
-export const DX = DB
-export default DB
+Noelle.load(DB)
 
