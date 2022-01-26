0fc213211274f8787a47c1201c63ea846a98bb8b
YueAo7 <leaoqi@outlook.com>
2022-01-26 18:41:53 +0800

    🎈 perf(编译核心): 优化代码
    现在可以自动导入到Noelle核心了

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 /*
  * @Date: 2022-01-21 16:30:21
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-25 18:43:27
- * @FilePath: \noelle-core-db\src\index.ts
+ * @LastEditTime: 2022-01-26 18:39:14
+ * @FilePath: \ComputeCoreDataBase\src\index.ts
  */
 import Noelle from "noelle-core"
-const t =  require.context("./",true,/^(?!index).*\.ts/)
-let DBT = {length:0}
-t.keys().map(item=>{
-    DBT={...DBT,...t(item)}  
+const t = require.context("./", true, /^(?!index).*\.ts/)
+const DB: any[]=[]
+t.keys().map(item => {
+    DB.push(...Object.values(t(item)))
 })
-const DB:any[] = []
-Object.keys(DBT).map(item=>{
-    DB.push(DBT[item])
-});
 Noelle.load(DB)
 
